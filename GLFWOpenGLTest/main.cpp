e36de8409ebdb67167a5d9530ac86607832db473
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-01-04 09:56:26 +0200

    project started, window created

---
+++ GLFWOpenGLTest/main.cpp
@@ -0,0 +1,76 @@
+// GLEW (NOTICE: GLEW MUST BE ALWAYS INCLUDED BEFORE GLFW)
+#define GLEW_STATIC
+#include <GL\glew.h>
+// GLFW
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+// Window dimensions
+const GLuint WIDTH = 800, HEIGHT = 600;
+// Functions
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
+
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL3.3" << std::endl;
+	// Init GLFW
+	glfwInit();
+	// Setting all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// Create a GLFWwindows object that we can use for GLFW's funtions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "OpenGL Tutorial", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+	
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	
+	// Initialize GLEW to setup the OpenGL Function pointers
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initialize GLEW" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		glfwPollEvents();
+
+		// Rendering commanrd here
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		// Swap the screen buffers
+		glfwSwapBuffers(window);
+	}
+	// Clearing any resources allocated by GLFW
+	glfwTerminate();
+	return 0;
+}
+
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	// When a user presses the escape key, we set the WindowShouldClose property to true, 
+	// closing the application
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+}
