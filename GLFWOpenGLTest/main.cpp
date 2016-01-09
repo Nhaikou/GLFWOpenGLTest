25b16b24d099a9357a9bec4882eacf951e794315
Niko Kinnunen <nikokinnunen92@gmail.com>
2016-01-09 16:48:12 +0200

    diffuse and specular maps

--- GLFWOpenGLTest/main.cpp
+++ GLFWOpenGLTest/main.cpp
@@ -34,7 +34,7 @@ bool keys[1024];
 bool firstMouse = true;
 
 // Position of the light/lamp
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(1.2f, 0.5f, 1.0f);
 
 // Deltatime
 GLfloat deltaTime = 0.0f;
@@ -251,8 +251,10 @@ int main()
 	GLuint texture;
 	GLuint texture2;
 
-	glGenTextures(1, &texture);
-	glBindTexture(GL_TEXTURE_2D, texture);
+	GLuint diffuseMap;
+
+	glGenTextures(1, &diffuseMap);
+	glBindTexture(GL_TEXTURE_2D, diffuseMap);
 
 	// Set the texture wrapping parameters
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -274,8 +276,11 @@ int main()
 	// Second texture
 	png.clear();
 	pixels.clear();
-	glGenTextures(1, &texture2);
-	glBindTexture(GL_TEXTURE_2D, texture2);
+
+	GLuint specularMap;
+
+	glGenTextures(1, &specularMap);
+	glBindTexture(GL_TEXTURE_2D, specularMap);
 
 	//// Set our texture parameters
 	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -311,15 +316,15 @@ int main()
 		// Load shaders
 		ourShader.Use();
 		
-		//// Binding texture
-		//glActiveTexture(GL_TEXTURE0);
-		//glBindTexture(GL_TEXTURE_2D, texture);
-		//glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture1"), 0);
+		// Binding texture
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, diffuseMap);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "material.diffuse"), 0);
 
-		////.... texture2
-		//glActiveTexture(GL_TEXTURE1);
-		//glBindTexture(GL_TEXTURE_2D, texture2);
-		//glUniform1i(glGetUniformLocation(ourShader.Program, "ourTexture2"), 1);
+		//.... texture2
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, specularMap);
+		glUniform1i(glGetUniformLocation(ourShader.Program, "material.specular"), 1);
 		
 
 		// Use cooresponding shader when setting uniforms/drawing objects
@@ -329,31 +334,19 @@ int main()
 		glUniform3f(viewPosLoc, camera.Position.x, camera.Position.y, camera.Position.z);
 
 		// Object materials
-		GLint matAmbientLoc = glGetUniformLocation(ourShader.Program, "material.ambient");
-		GLint matDiffuseLoc = glGetUniformLocation(ourShader.Program, "material.diffuse");
+		//GLint matAmbientLoc = glGetUniformLocation(ourShader.Program, "material.ambient");
+		//GLint matDiffuseLoc = glGetUniformLocation(ourShader.Program, "material.diffuse");
 		GLint matSpecularLoc = glGetUniformLocation(ourShader.Program, "material.specular");
 		GLint matShineLoc = glGetUniformLocation(ourShader.Program, "material.shininess");
 
-		glUniform3f(matAmbientLoc, 1.0f, 0.5f, 0.31f);
-		glUniform3f(matDiffuseLoc, 1.0f, 0.5f, 0.31f);
-		glUniform3f(matSpecularLoc, 0.5f, 0.5f, 0.5f);
+		//glUniform3f(matAmbientLoc, 1.0f, 0.5f, 0.31f);
+		//glUniform3f(matDiffuseLoc, 1.0f, 0.5f, 0.31f);
+		//glUniform3f(matSpecularLoc, 0.5f, 0.5f, 0.5f);
 		glUniform1f(matShineLoc, 32.0f);
 		
 		// Light materials
-		GLint lightAmbientLoc = glGetUniformLocation(ourShader.Program, "light.ambient");
-		GLint lightDiffuseLoc = glGetUniformLocation(ourShader.Program, "light.diffuse");
-		GLint lightSpecularLoc = glGetUniformLocation(ourShader.Program, "light.specular");
-		
-		glm::vec3 lightColor;
-		lightColor.x = sin(glfwGetTime() * 2.0f);
-		lightColor.y = sin(glfwGetTime() * 0.7f);
-		lightColor.z = sin(glfwGetTime() * 1.3f);
-
-		glm::vec3 diffuseColor = lightColor * glm::vec3(0.5f);
-		glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
-
-		glUniform3f(glGetUniformLocation(ourShader.Program, "light.ambient"), ambientColor.x, ambientColor.y, ambientColor.z);
-		glUniform3f(glGetUniformLocation(ourShader.Program, "light.diffuse"), diffuseColor.x, diffuseColor.y, diffuseColor.z);
+		glUniform3f(glGetUniformLocation(ourShader.Program, "light.ambient"), 0.2f, 0.2f, 0.2f);
+		glUniform3f(glGetUniformLocation(ourShader.Program, "light.diffuse"), 0.5f, 0.5f, 0.5f);
 		glUniform3f(glGetUniformLocation(ourShader.Program, "light.specular"), 1.0f, 1.0f, 1.0f);
 
 		// Light color
@@ -382,7 +375,7 @@ int main()
 		// Draw box
 		glBindVertexArray(VAO);
 		glm::mat4 model;
-		model = glm::rotate(model, (GLfloat)glfwGetTime() + glm::radians(50.0f), glm::vec3(1.0f, 0.3f, 05.f));
+		//model = glm::rotate(model, (GLfloat)glfwGetTime() + glm::radians(50.0f), glm::vec3(1.0f, 0.3f, 05.f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
@@ -406,9 +399,9 @@ int main()
 		glBindVertexArray(0);
 
 		// Lamp movement
-		lightPos.x = 1.0f + sin(glfwGetTime()) * 2.0f;
-		lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
-		lightPos.z = lightPos.x - lightPos.y;
+		//lightPos.x = 1.0f + sin(glfwGetTime()) * 1.0f;
+		//lightPos.y = sin(glfwGetTime() / 2.0f) * 1.0f;
+		//lightPos.z = lightPos.x - lightPos.y;
 
 
 		/*for (GLuint i = 0; i < 10; i++)
